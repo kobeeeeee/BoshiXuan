93aebe2eaa7019509ff00918091677674e7e419e
kobeeeeee <386793119@qq.com>
2016-07-03 19:51:28 +0800

    登录画面跳转测试提交

--- BoShiXuan/app/src/main/java/www/chendanfeng/com/boishixuan/LoginActivity.java
+++ BoShiXuan/app/src/main/java/www/chendanfeng/com/boishixuan/LoginActivity.java
@@ -1,7 +1,9 @@
 package www.chendanfeng.com.boishixuan;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
@@ -13,6 +15,9 @@ import butterknife.ButterKnife;
 import www.chendanfeng.com.util.LogUtil;
 
 public class LoginActivity extends BaseActivity {
+    public static final int TYPE_LOGIN = 1;
+    public static final int TYPE_FORGET = 2;
+    public static final int TYPE_REGISTER = 3;
     @Bind(R.id.login_user)
     EditText userEditText;
     @Bind(R.id.login_password)
@@ -28,6 +33,33 @@ public class LoginActivity extends BaseActivity {
         LogUtil.init(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ButterKnife.bind(this);;
+        ButterKnife.bind(this);
+        registerTextView.setOnClickListener(new MyOnClickListener(TYPE_REGISTER));
+        loginButton.setOnClickListener(new MyOnClickListener(TYPE_LOGIN));
+        forgetTextView.setOnClickListener(new MyOnClickListener(TYPE_FORGET));
+    }
+    class MyOnClickListener implements  View.OnClickListener {
+        public int mType;
+        public MyOnClickListener(int type) {
+            this.mType = type;
+        }
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            switch (this.mType) {
+                case TYPE_LOGIN:
+                    //TODO 测试跳转主页面代码
+                    intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    LoginActivity.this.finish();
+                    break;
+                case TYPE_FORGET:
+                    break;
+                case TYPE_REGISTER:
+                    intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
     }
 }
