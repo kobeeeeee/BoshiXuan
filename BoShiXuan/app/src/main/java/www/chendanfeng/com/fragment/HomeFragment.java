7d15e80bdfe8260bbf7730ff77abc0ea4c31aa20
kobeeeeee <386793119@qq.com>
2016-07-05 23:54:39 +0800

    租赁画面做成

--- BoShiXuan/app/src/main/java/www/chendanfeng/com/fragment/HomeFragment.java
+++ BoShiXuan/app/src/main/java/www/chendanfeng/com/fragment/HomeFragment.java
@@ -1,5 +1,7 @@
 package www.chendanfeng.com.fragment;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -19,15 +21,25 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import www.chendanfeng.com.adapter.BannerViewPagerAdapter;
+import www.chendanfeng.com.boishixuan.LeaseActivity;
 import www.chendanfeng.com.boishixuan.R;
 
 /**
  * Created by Administrator on 2016/7/2 0002.
  */
 public class HomeFragment extends BaseFragment{
+    public static final int TYPE_LEASE = 1;
+    public static final int TYPE_WITHDRAW = 2;
+    public static final int TYPE_RECHARGE = 3;
+    public static final int TYPE_DEPOSIT = 4;
+    public static final int TYPE_ORDER = 5;
+    public static final int TYPE_MORE = 6;
+
     @Bind(R.id.tv_head)
     TextView mHeader;
+    //轮播图初始位置
     private int mPosition = 1;
+    //当手指触碰到时，设置为false
     private boolean mIsContinue = true;
     private View mView;
     @Bind(R.id.left_dot)
@@ -38,6 +50,20 @@ public class HomeFragment extends BaseFragment{
     ImageView mRightDot;
     @Bind(R.id.vpInfoBanner)
     ViewPager mViewBanner;
+
+    @Bind(R.id.leaseImage)
+    ImageView mLeaseImage;
+    @Bind(R.id.withdrawImage)
+    ImageView mWithdrawImage;
+    @Bind(R.id.rechargeImage)
+    ImageView mRechargeImage;
+    @Bind(R.id.depositImage)
+    ImageView mDepositeImage;
+    @Bind(R.id.orderImage)
+    ImageView mOrderImage;
+    @Bind(R.id.moreImage)
+    ImageView mMoreImage;
+
     List<ImageView> mAdvancePicList = new ArrayList<>();
     private ViewPager.LayoutParams mParams;
     private List<ImageView> mDotList = new ArrayList<>();
@@ -60,6 +86,7 @@ public class HomeFragment extends BaseFragment{
         initDot();
         initViewPager();
         initHeader();
+        initClickListener();
     }
 
     private void initHeader() {
@@ -162,7 +189,15 @@ public class HomeFragment extends BaseFragment{
         }
 
     }
-
+    //初始化下边六个按钮的点击事件
+    private void initClickListener() {
+        this.mLeaseImage.setOnClickListener(new MyOnClickListener(TYPE_LEASE));
+        this.mWithdrawImage.setOnClickListener(new MyOnClickListener(TYPE_WITHDRAW));
+        this.mRechargeImage.setOnClickListener(new MyOnClickListener(TYPE_RECHARGE));
+        this.mDepositeImage.setOnClickListener(new MyOnClickListener(TYPE_DEPOSIT));
+        this.mOrderImage.setOnClickListener(new MyOnClickListener(TYPE_ORDER));
+        this.mMoreImage.setOnClickListener(new MyOnClickListener(TYPE_MORE));
+    }
     private Thread mThread = new Thread(new Runnable() {
 
         @Override
@@ -219,6 +254,32 @@ public class HomeFragment extends BaseFragment{
         super.onPause();
         if(this.mThread != null) {
             this.mThread.interrupt();
+        }
+    }
+    class MyOnClickListener implements View.OnClickListener {
+        private int mType;
+        public MyOnClickListener(int type) {
+            this.mType = type;
+        }
+        @Override
+        public void onClick(View v) {
+            Intent intent;
+            switch (this.mType) {
+                case TYPE_LEASE:
+                    intent = new Intent(getActivity(), LeaseActivity.class);
+                    startActivity(intent);
+                    break;
+                case TYPE_WITHDRAW:
+                    break;
+                case TYPE_RECHARGE:
+                    break;
+                case TYPE_DEPOSIT:
+                    break;
+                case TYPE_ORDER:
+                    break;
+                case TYPE_MORE:
+                    break;
+            }
         }
     }
 }
