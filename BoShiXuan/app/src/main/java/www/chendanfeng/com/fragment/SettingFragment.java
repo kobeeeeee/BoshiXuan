26b9b8f8433df57bbdbe53ad9b6d3350b89a37f8
renaaaa <904751306@qq.com>
2016-07-17 02:14:27 +0800

    拨打电话

--- BoShiXuan/app/src/main/java/www/chendanfeng/com/fragment/SettingFragment.java
+++ BoShiXuan/app/src/main/java/www/chendanfeng/com/fragment/SettingFragment.java
@@ -1,8 +1,11 @@
 package www.chendanfeng.com.fragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,8 +28,12 @@ import www.chendanfeng.com.boishixuan.RegisterActivity;
 public class SettingFragment extends BaseFragment{
     private View mView;
     public static final int TYPE_ABOUT = 1;
+    public static final int TYPE_SHARE = 2;
+    public static final int TYPE_PHONE = 3;
     @Bind(R.id.aboutus)
     TextView aboutusText;
+    @Bind(R.id.phone)
+    TextView phoneText;
 
     @Nullable
     @Override
@@ -34,6 +41,7 @@ public class SettingFragment extends BaseFragment{
         this.mView = inflater.inflate(R.layout.fragment_setting, container, false);
         ButterKnife.bind(this, this.mView);
         aboutusText.setOnClickListener(new MyOnClickListener(TYPE_ABOUT));
+        phoneText.setOnClickListener(new MyOnClickListener(TYPE_PHONE));
         return this.mView;
     }
 
@@ -44,13 +52,41 @@ public class SettingFragment extends BaseFragment{
         }
         @Override
         public void onClick(View v) {
-            Intent intent;
+            Intent intent = new Intent();
             switch (this.mType) {
                 case TYPE_ABOUT:
                     //TODO 测试跳转主页面代码
                     intent = new Intent(getActivity(),AboutActivity.class);
                     startActivity(intent);
                     break;
+                case TYPE_SHARE:
+                    break;
+                case TYPE_PHONE:
+                    final String phone =  phoneText.getText().toString();
+                    StringBuffer message = new StringBuffer();
+                    message.append("确定要拨打").append(phone).append("么？");
+                    AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+                    builder.setTitle("温馨提示");
+                    //builder.setMessage("确定要拨打4008-597-333么？");
+                    builder.setMessage(message.toString());
+                    builder.setNegativeButton("取消",new DialogInterface.OnClickListener(){
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+                           dialog.dismiss();
+                       }
+                   });
+                    builder.setPositiveButton("拨打",new DialogInterface.OnClickListener(){
+                       @Override
+                       public void onClick(DialogInterface dialog, int which) {
+                           dialog.dismiss();
+                           Intent intent = new Intent();
+                           intent.setAction(Intent.ACTION_CALL);
+                           intent.setData(Uri.parse("tel:" + phone));
+                           //开启系统拨号器
+                           startActivity(intent);
+                       }
+                   });
+                    builder.create().show();
             }
         }
     }
