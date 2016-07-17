ff21c10a3a9908d19406d2a3f97d228b88206496
kobeeeeee <386793119@qq.com>
2016-07-17 23:06:08 +0800

    response  样例提交

---
+++ BoShiXuan/app/src/main/java/www/chendanfeng/com/network/model/userInfoResponse.java
@@ -0,0 +1,13 @@
+package www.chendanfeng.com.network.model;
+
+import www.chendanfeng.com.network.BaseResponse;
+
+/**
+ * Created by Administrator on 2016/7/17 0017.
+ */
+public class userInfoResponse extends BaseResponse{
+    public String user_id;
+    public String user_phone;
+    public String user_name;
+    public String is_verify;
+}
