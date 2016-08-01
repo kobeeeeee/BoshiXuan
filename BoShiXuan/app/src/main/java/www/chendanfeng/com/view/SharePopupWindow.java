7dfcfdbb8849e9eecd875cbdeb717cfa2924b6da
kobeeeeee <386793119@qq.com>
2016-08-01 23:33:58 +0800

    代码提交

---
+++ BoShiXuan/app/src/main/java/www/chendanfeng/com/view/SharePopupWindow.java
@@ -0,0 +1,43 @@
+package www.chendanfeng.com.view;
+
+import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+
+import www.chendanfeng.com.boishixuan.R;
+
+/**
+ * Created by yangln10784 on 2016/8/1.
+ */
+public class SharePopupWindow extends PopupWindow {
+    private View shareView;
+    public SharePopupWindow(Activity context, View.OnClickListener itemclick){
+        super(context);
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        shareView = inflater.inflate(R.layout.activity_share, null);
+        this.setWidth(RelativeLayout.LayoutParams.MATCH_PARENT);
+        this.setHeight(RelativeLayout.LayoutParams.MATCH_PARENT);
+        this.setContentView(shareView);
+        this.setBackgroundDrawable(new ColorDrawable());
+        this.setFocusable(true);
+        this.setOutsideTouchable(true);
+        this.setAnimationStyle(R.style.ShareAnimation);
+        shareView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int height = shareView.findViewById(R.id.popLayout).getTop();
+                int y = (int)event.getY();
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    if(y < height){
+                        dismiss();
+                    }
+                }
+                return true;
+            }
+        });
+    }
+}
